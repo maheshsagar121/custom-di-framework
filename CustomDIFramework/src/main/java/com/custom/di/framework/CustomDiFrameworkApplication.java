d7a845d04dc9d72c16a73f329a00555ece6a2b7d
mahesh <maheshsagar121@gmail.com>
2020-05-07 14:40:14 +0800

    CustomDIFrameWork: first checkin for project structure

---
+++ CustomDIFramework/src/main/java/com/custom/di/framework/CustomDiFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.custom.di.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomDiFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomDiFrameworkApplication.class, args);
+	}
+
+}
